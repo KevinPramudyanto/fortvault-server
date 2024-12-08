43d74ed2fbb45f944fd3d6758002473ce1fa4e8f
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-12-08 21:20:53 +0800

    Create socketio functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_socketio import SocketIO
 from resources.auth import auth
 from resources.tool import tool
 from resources.user import user
@@ -10,10 +11,16 @@ app = Flask(__name__)
 CORS(app)
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 jwt = JWTManager(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 app.register_blueprint(auth)
 app.register_blueprint(tool)
 app.register_blueprint(user)
 
+@socketio.on('message')
+def handle_message(input):
+    print('received message: ' + input)
+    socketio.emit('message', input)
+
 if __name__ == '__main__':
-    app.run(port=5001,debug=True)
+    socketio.run(app, port=5001, debug=True, allow_unsafe_werkzeug=True)

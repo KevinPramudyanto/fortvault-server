ed23ece1a99b6e0c41ed0c2dd06475f0542ced1c
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-12-10 10:45:23 +0800

    Create change_password api

--- resources/auth.py
+++ resources/auth.py
@@ -57,10 +57,47 @@ def signin():
 
         claims = { 'id': user['id'], 'username': user['username'], 'role': user['role'], 'manager': user['manager'] }
         token = create_access_token(identity=user['id'], additional_claims=claims, expires_delta=timedelta(hours=1))
-        return jsonify({ 'token': token, 'id': user['id'], 'role': user['role'] }), 200
+        return jsonify({ 'token': token, 'id': user['id'], 'username': user['username'], 'role': user['role'] }), 200
     except Exception as error:
         print(error)
         return jsonify({ 'message': 'Failed to signin user.' }), 500
+    finally:
+        if conn:
+            release_connection(conn)
+
+
+@auth.route('/api/changepassword', methods=['PATCH'])
+@jwt_required()
+def change_password():
+    conn = None
+    try:
+        jwt_user = get_jwt()
+        if jwt_user['role'] != 'manager' and jwt_user['role'] != 'worker':
+            return jsonify({ 'message': 'You are not manager or worker.' }), 403
+
+        req = request.get_json()
+        if len(req['oldPassword']) < 1 or len(req['oldPassword']) > 20:
+            return jsonify({ 'message': 'Password must be between 1-20 characters.' }), 400
+        if len(req['newPassword']) < 1 or len(req['newPassword']) > 20:
+            return jsonify({ 'message': 'Password must be between 1-20 characters.' }), 400
+
+        conn, cursor = get_connection()
+        cursor.execute('SELECT id, password FROM users WHERE id=%s', (jwt_user['id'],))
+        user_one = cursor.fetchone()
+        if not user_one:
+            return jsonify({ 'message': 'No user found.' }), 404
+
+        is_valid_password = bcrypt.checkpw(req['oldPassword'].encode('utf-8'), user_one['password'].encode('utf-8'))
+        if not is_valid_password:
+            return jsonify({ 'message': 'Current password is incorrect.' }), 403
+
+        hashed_password = bcrypt.hashpw(req['newPassword'].encode('utf-8'), bcrypt.gensalt())
+        cursor.execute('UPDATE users SET password=%s WHERE id=%s', (hashed_password.decode('utf-8'), jwt_user['id']))
+        conn.commit()
+        return jsonify({ 'message': 'Password changed.' }), 200
+    except Exception as error:
+        print(error)
+        return jsonify({ 'message': 'Failed to change password.' }), 500
     finally:
         if conn:
             release_connection(conn)

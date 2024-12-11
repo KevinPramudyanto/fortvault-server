52b83d2d9b61ed2db4015a33c64f663632c5ff7f
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-12-11 16:47:00 +0800

    Remove socketio and its functionality

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from flask_socketio import SocketIO
 from resources.seed import seed
 from resources.auth import auth
 from resources.tool import tool
@@ -13,7 +12,6 @@ app = Flask(__name__)
 CORS(app)
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 jwt = JWTManager(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
 
 app.register_blueprint(seed)
 app.register_blueprint(auth)
@@ -21,10 +19,5 @@ app.register_blueprint(tool)
 app.register_blueprint(user)
 app.register_blueprint(logs)
 
-@socketio.on('message')
-def handle_message(input):
-    print('received message: ' + input)
-    socketio.emit('message', input)
-
 if __name__ == '__main__':
-    socketio.run(app, port=5001, debug=True, allow_unsafe_werkzeug=True)
+    app.run(port=5001, debug=True)
